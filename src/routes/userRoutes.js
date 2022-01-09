6a35b8092028bea59bc6f7e8d77206e7b087f859
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2022-01-08 23:42:55 -0300

    register route created

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,4 +3,16 @@ const routes = require('express').Router()
 const User = require('../models/User')
 
 
+routes.post('/register', async (req, res) => {
+
+    try {
+        const user = await User.create(req.body)
+
+        res.send({ user })
+    } catch (error) {
+        return res.status(400).send({ error: 'something is wrong.' })
+    }
+
+})
+
 module.exports = routes
